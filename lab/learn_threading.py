65c42e918e3f64be5a67d0327552ed0e8ac55d68
Joseph McCullough <joseph@vertstudios.com>
2011-11-09 23:44:11 -0600

    Refactored threading example to not need global variable

--- lab/learn_threading.py
+++ lab/learn_threading.py
@@ -5,28 +5,29 @@ import sys
 
 EXIT_FLAG = 0
 
-def run_timer():
-	global EXIT_FLAG
+def run_timer(theThread):
+	'''
+	Displays a timer that counts up while theThread is active
+	theThread is a thread waiting on user input to stop the timer.
+	'''
 
 	seconds = 0
-	while EXIT_FLAG==0:
-		time.sleep(1)
+	while theThread.is_alive():
 		timeFmt = datetime.timedelta(seconds=seconds)
 
 		# Prevent extra write on exit
-		if EXIT_FLAG == 0:
+		if theThread.is_alive():
 			sys.stdout.write("\r%s" % timeFmt)
 			sys.stdout.flush()
 			seconds += 1
+			time.sleep(1)
 
 def get_input():
-	global EXIT_FLAG
 	print "Press something!"
 	raw_input()
-	EXIT_FLAG = 1
 
 inputThread = threading.Thread(target=get_input)
-timerThread = threading.Thread(target=run_timer)
+timerThread = threading.Thread(target=run_timer, args=(inputThread,))
 
 inputThread.start()
 timerThread.start()
